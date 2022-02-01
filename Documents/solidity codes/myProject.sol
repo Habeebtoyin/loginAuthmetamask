1d9ef660ddc462d68e836e3b3bb8f6b75e8c9a3c
AREMSON <habeebtoyin2021@gmail.com>
2022-02-01 16:46:31 +0100

    file

---
+++ Documents/solidity codes/myProject.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.6.0;
+
+contract CoolNumberContract {
+    uint public coolNumber= 10;
+    function setcoolNumber(uint _coolNumber) public {
+        coolNumber=_coolNumber;
+    }
+}
+//C:\Users\HABEEB\Desktop\solidity codes\node_modules\web3\dist\web3.min.js
